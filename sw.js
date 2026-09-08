b249a909538eabf18dc4418da6014311548ce6aa
petrattocolin-pixel <petrattocolin@gmail.com>
2026-09-08 14:59:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'carnet-de-chasse-v1';
+const APP_SHELL = ['./carnet-de-chasse.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  // Ne gère que les requêtes GET de même origine (laisse passer les tuiles de carte, etc.)
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const network = fetch(req).then((res) => {
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
